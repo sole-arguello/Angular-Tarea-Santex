4c241c94135fa383aee907c9804cc769b731157c
Arguello Soledad <soledad.ar1@gmail.com>
2023-07-15 20:26:32 -0300

    modularizo angular material y componente boton

--- proyectoAngularSantex/src/app/app.module.ts
+++ proyectoAngularSantex/src/app/app.module.ts
@@ -4,17 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+
+//components
+import { ButtonsComponent } from './buttons/buttons.component';
+
+//angular material
+import { MaterialModule } from '../app/material/material.module'; 
+
+import { HttpClientModule } from '@angular/common/http';
+import { TableComponent } from './table/table.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    TableComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MaterialModule,
+    HttpClientModule
 
   ],
   providers: [],
