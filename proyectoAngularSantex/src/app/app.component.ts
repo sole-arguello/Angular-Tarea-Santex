688e4c50dc4162210b37c886b7e581bd1b33e4ac
Arguello Soledad <soledad.ar1@gmail.com>
2023-07-15 20:08:19 -0300

    creo el type custom y  el array

--- proyectoAngularSantex/src/app/app.component.ts
+++ proyectoAngularSantex/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+type Products = {
+  id: number
+  name: string
+  price: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectoAngularSantex';
+
+  showList:boolean = true
+  message:string = ''
+  statusButton:string = ''
+  
+  products: Products [] = [
+    { "id": 1, "name": "Harina", "price": "$ 300" }, 
+    { "id": 2, "name": "Azucar", "price": "$ 600" },
+    { "id": 3, "name": "Yerba", "price": "$ 150" }
+
+  ]
+
 }
